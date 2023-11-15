2b895056f2d8518a2b19876fcac211a2dbc6e587
Erich Krieg <eri.krieg.22@lehre.mosbach.dhbw.de>
2023-11-15 18:50:48 +0100

    Refactoring v-if statements and Open-Meteo Fetch

--- src/main/client-app/src/components/MainComponent.vue
+++ src/main/client-app/src/components/MainComponent.vue
@@ -10,28 +10,28 @@
   <form id="loginform" @submit.prevent="login">
     <input type="text" v-model="username" id="query" name="login_username" placeholder="Benutzername"/>
     <input type="text" v-model="password" id="query" name="login_password" placeholder="Passwort"/>
-    <button id="button" type="submit">Anmelden</button>
-    <button id="button" type="button" @click="register">Registrieren</button>
-    <button id="button" type="button" @click="logout">Ausloggen</button>
+    <button id="button" type="submit" v-if="!loggedIn">Anmelden</button>
+    <button id="button" type="button" v-if="!loggedIn" @click="register">Registrieren</button>
+    <button id="button" type="button" @click="logout" v-if="loggedIn">Ausloggen</button>
   </form>
   <div id="divButtons">
     <label for="query"></label><input v-model="query" @keyup.enter="askElsewhere(query)" type="text" id="query"
                                       class="form-control" placeholder="Wie ist das Wetter anderswo?">
     <button type="button" id="button" @click="askElsewhere(query)">Suche</button>
-    <button type="button" id="button" v-if="newLocal" @click="askLocal">Wie ist das aktuelle Wetter bei mir?</button>
+    <button type="button" id="button" v-if="notLocal" @click="askLocal">Wie ist das aktuelle Wetter bei mir?</button>
   </div>
   <div id="div" v-if="boxVisible">
-    <p id="process_status" v-if="processing">{{ processStatus }}</p>
+    <p id="process_status" v-if="!dataSuccess">{{ processStatus }}</p>
     <p id="reverse_geocoding" v-if="dataSuccess">Aktueller Standort: {{ reverseGeocoding }}</p>
     <p id="current_temperature" v-if="dataSuccess">Aktuelle Temperatur: {{ currentTemperature }}</p>
     <p id="current_windspeed" v-if="dataSuccess">Aktuelle Windgeschwindigkeit: {{ currentWindspeed }}</p>
     <p id="current_winddirection" v-if="dataSuccess">Aktuelle Windrichtung: {{ currentWinddirection }}</p>
     <p id="current_weathercode" v-if="dataSuccess">Aktueller Wettercode: {{ currentWeathercode }}</p>
-    <button type="button" id="button" v-if="oldQuery" @click="askElsewhere(lastQuery)">Gesuchtes Wetter
+    <button type="button" id="button" v-if="notLocal && dataSuccess && noFavorite" @click="fetchWeather(queryLat, queryLon)">Gesuchtes Wetter
       aktualisieren
     </button>
-    <button type="button" id="button" v-if="oldLocal && dataSuccess" @click="askLocal">Lokales Wetter aktualisieren</button>
-    <button type="button" id="button" v-if="oldFavorite && dataSuccess && loggedIn" @click="askFavorite(lastFavoriteLat, lastFavoriteLon)">
+    <button type="button" id="button" v-if="!notLocal && dataSuccess && noFavorite" @click="askLocal">Lokales Wetter aktualisieren</button>
+    <button type="button" id="button" v-if="!noFavorite && dataSuccess && loggedIn" @click="askFavorite(lastFavoriteLat, lastFavoriteLon)">
       Favorisiertes Wetter aktualisieren
     </button>
     <button type="button" id="button" v-if="noFavorite && dataSuccess && loggedIn" @click="addFavorite">Standort favorisieren</button>
@@ -58,26 +58,23 @@ const currentWindspeed = ref('')
 const currentWinddirection = ref('')
 const currentWeathercode = ref('')
 const boxVisible = ref(false)
-const newLocal = ref(true)
-const oldLocal = ref(false)
 const query = ref("")
-const lastQuery = ref("")
-const oldQuery = ref(false)
 const currentlat = ref("")
 const currentlon = ref("")
 const dataSuccess = ref(false)
-const processing = ref(true)
 const favoriteName = ref("")
 const favorites = ref([])
 const lastFavoriteLat = ref("")
 const lastFavoriteLon = ref("")
-const oldFavorite = ref(false)
 const currentUser = ref("")
 const loggedIn = ref(false)
 const username = ref("")
 const password = ref("")
 const noFavorite = ref(true)
 const lastFavoriteId = ref("")
+const notLocal = ref(true)
+const queryLat = ref("")
+const queryLon = ref("")
 
 async function login() {
   const formData = new FormData
@@ -153,86 +150,64 @@ function logout(){
 
 async function askFavorite(lat, lon, id) {
   noFavorite.value = false
-  oldFavorite.value = false
+  notLocal.value = true
   boxVisible.value = true
-  processing.value = true
-  oldLocal.value = false
-  processStatus.value = "Suche..."
   dataSuccess.value = false
   lastFavoriteLat.value = lat
   lastFavoriteLon.value = lon
   lastFavoriteId.value = id
-  const response = await fetch(
-      'https://api.open-meteo.com/v1/forecast?latitude=' + lat
-      + '&longitude=' + lon + '&current_weather=true'
-  )
-  askWeather(response)
-  oldQuery.value = false
-  newLocal.value = false
-  oldLocal.value = false
-  oldFavorite.value = true
+  await fetchWeather(lat, lon)
 }
 
 function askLocal() {
   noFavorite.value = true
-  oldLocal.value = false
-  oldFavorite.value = false
-  oldQuery.value = false
-  processing.value = true
+  notLocal.value = false
   boxVisible.value = true
   if (!navigator.geolocation) {
     processStatus.value = 'Dein Standort kann leider nicht ermittelt werden'
     dataSuccess.value = false
   } else
-    processStatus.value = "Suche..."
   dataSuccess.value = false
   navigator.geolocation.getCurrentPosition(async (pos) => {
     console.log(pos)
-    const response = await fetch(
-        'https://api.open-meteo.com/v1/forecast?latitude=' + pos.coords.latitude
-        + '&longitude=' + pos.coords.longitude + '&current_weather=true'
-    )
-    askWeather(response)
-    newLocal.value = false
-    oldLocal.value = true
-    oldQuery.value = false
+    await fetchWeather(pos.coords.latitude, pos.coords.longitude)
+  }, () => {
+    processStatus.value = 'Dein Standort kann leider nicht ermittelt werden'
+    dataSuccess.value = false
+    notLocal.value = true
+  })
+}
+
+function askElsewhere(query) {
+  noFavorite.value = true
+  notLocal.value = true
+  dataSuccess.value = false
+  boxVisible.value = true
+  search(query, async (lat, lon) => {
+    console.log(query)
+    await fetchWeather(lat, lon)
   }, () => {
     processStatus.value = 'Dein Standort kann leider nicht ermittelt werden'
     dataSuccess.value = false
   })
 }
 
-function askElsewhere(query) {
-  noFavorite.value = true
+async function fetchWeather(latitude, longitude){
   processStatus.value = "Suche..."
-  oldFavorite.value = false
   dataSuccess.value = false
-  lastQuery.value = query
-  oldLocal.value = false
-  oldQuery.value = false
-  processing.value = true
-  boxVisible.value = true
-  search(query, async (lat, lon) => {
-    console.log(query)
-    const response = await fetch(
-        'https://api.open-meteo.com/v1/forecast?latitude=' + lat
-        + '&longitude=' + lon + '&current_weather=true'
-    )
-    askWeather(response)
-    oldQuery.value = true
-    newLocal.value = true
-    oldLocal.value = false
-  }, () => {
-    processStatus.value = 'Dein Standort kann leider nicht ermittelt werden'
-    dataSuccess.value = false
-  })
+  queryLat.value = latitude
+  queryLon.value = longitude
+  const response = await fetch(
+      'https://api.open-meteo.com/v1/forecast?latitude=' + latitude
+      + '&longitude=' + longitude + '&current_weather=true'
+  )
+  askWeather(response)
 }
 
 function askWeather(response) {
   if (response.ok) {
     response.json().then(async (data) => {
       await reverse_geocoding(data.latitude, data.longitude, (location_name, road_name, city_name) => {
-        processing.value = false
         reverseGeocoding.value = location_name
         favoriteName.value = road_name + city_name
         currentlat.value = data.latitude
@@ -245,9 +220,9 @@ function askWeather(response) {
       dataSuccess.value = true
     })
   } else {
-    processing.value = true
     processStatus.value = 'Die Wetterdaten können leider nicht ermittelt werden'
     dataSuccess.value = false
+    notLocal.value = true
   }
 }
 
@@ -272,6 +247,7 @@ async function addFavorite() {
       lastFavoriteId.value = favorite.id
     })
     noFavorite.value = false
+    notLocal.value = true
   }
 }
 
@@ -287,6 +263,7 @@ async function removeFavorite(id) {
     console.log(response)
     await getFavorites()
     noFavorite.value = true
+    notLocal.value = true
   }
 }
 </script>
@@ -336,42 +313,6 @@ body {
   border-style: solid;
   border-color: black;
   border-radius: 6px;
-}
-
-#refresh {
-  border-color: black;
-  border-radius: 6px;
-  border-style: solid;
-  background-color: white;
-  font-family: sans-serif;
-  color: black;
-  margin: auto auto 10px auto;
-  height: 40px;
-  width: 250px;
-}
-
-#search_refresh {
-  border-color: black;
-  border-radius: 6px;
-  border-style: solid;
-  background-color: white;
-  font-family: sans-serif;
-  color: black;
-  margin: auto auto 10px auto;
-  height: 40px;
-  width: 250px;
-}
-
-#search_refresh:hover {
-  border-color: white;
-  background-color: black;
-  color: white;
-}
-
-#refresh:hover {
-  border-color: white;
-  background-color: black;
-  color: white;
 }
 
 #query {
